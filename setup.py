93aad0959d663e742195c0ef371933d2d7d43acc
Francesco Maida <francesco.maida@gmail.com>
2020-01-19 15:33:00 +0100

    Minor modification to the setup.py file

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from distutils.core import setup
 
 setup(
     name="mkdocs-blog-plugin",
-    version="0.1.0",
+    version="0.1.1",
     author="Francesco Maida",
     author_email="francesco.maida@gmail.com",
     packages=["mkdocs_blog"],
+    url='https://github.com/fmaida/mkdocs-blog-plugin',
     license="LICENSE.txt",
     description="Keeps a really simple blog in your mkdocs",
     install_requires=[
